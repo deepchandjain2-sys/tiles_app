23f87a411da57e02b4cf713244da99b0b3fe8c89
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-08-20 23:41:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ st.markdown("""
 # -------------------------------------------------------------
 # 2. GOOGLE SHEET LIVE CSV CONNECTOR (BUSY STOCK)
 # -------------------------------------------------------------
-GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRMgTzS4kNWfaIOByOAZ-RS_XQP7zqiKXAgEkgVhrHNYQU5Jn-srXAfuOW_yPcAmW1G_FrEa59S-RyJ/pub?gid=0&single=true&output=csv"
+GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/14lY-SKjwd9hins1gSp6lR1C4_AOWOx2an8c-UgKaPY/export?format=csv&gid=0"
 
 @st.cache_data(ttl=60)
 def fetch_busy_inventory():
