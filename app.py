4f96f37a46d52b129eb040ac74d134bca482fded
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-08-22 19:41:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,10 @@ if "items" not in st.session_state:
 # -------------------------------------------------------------
 # GOOGLE SHEET LIVE STOCK LOADER (ITEM MASTER FORMAT)
 # -------------------------------------------------------------
-  GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/1qhlBmCLldDAkQMXrbYKSrFcEhYvFdXv2XIABLxO6pA/export?format=csv&gid=0"def load_busy_inventory():
+GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/1qhlBmCLldDAkQMXrbYKSrFcEhYvFdXv2XIABLxO6pA/export?format=csv&gid=0"
+
+@st.cache_data(ttl=30)
+def load_busy_inventory():
     try:
         df = pd.read_csv(GOOGLE_SHEET_CSV_URL)
         if df.empty:
