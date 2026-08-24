f065cd87dffdf4d83f848b5e5edbc8f64e3cb30a
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-08-24 23:09:50 +0530

    Fix typo in print statement

--- app.py
+++ app.py
@@ -282,7 +282,7 @@ elif st.session_state.current_nav == "2 Tiles Selection (Area-Wise)":
             if "my_selected_tiles" not in st.session_state or not isinstance(st.session_state.my_selected_tiles, list):
                 st.session_state.my_selected_tiles = []
                 
-            customer_items = [i for i in st.session_state.my_selected_tiles if isinstance(i, dict) and i.get("cid"] == cid]
+           customer_items = [i for i in st.session_state.my_selected_tiles if isinstance(i, dict) and i.get("cid") == cid]
             
             if customer_items:
                 item_to_remove = None
