01405f08ab9718779cc39672b3e7211cfb8006e0
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-06 15:04:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 import streamlit as st
 from fpdf import FPDF
 from datetime import datetime
-from database import push_db_to_github
+
 
 st.set_page_config(
     page_title="Jay Granite & Tiles Hub",
