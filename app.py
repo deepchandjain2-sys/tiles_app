bb3a1805b3a215f0b892442854156617c05ae5fb
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-06 20:07:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -612,7 +612,7 @@ elif nav == "3️⃣ Sq.Ft Entry & Final Estimate":
 
     wa_msg += f"📦 *Grand Total Area:* {tot_sq:.2f} Sq.Ft\n"
     wa_msg += f"📦 *Grand Total Boxes:* {tot_bx:.0f} Boxes\n"
-    wa_msg += f"Thank you for choosing Jay Granite & Tiles!"
+    wa_msg += f"please update physical stock as possible!"
     st.text_area("Message Preview:", value=wa_msg, height=180)
 
     pdf_bytes = generate_pdf_quotation(curr_c, curr_c["selections"])
