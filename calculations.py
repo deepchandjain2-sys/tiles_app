b6095ecba4d45a92735c3fbbaafd3d8bfa160bc0
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-10 19:46:25 +0530

    Update calculations.py

--- calculations.py
+++ calculations.py
@@ -1,30 +1,22 @@
 import math
 
-def calculate_box_sqft(con_factor, packing_unit):
-    """
-    1 बॉक्स का कुल क्षेत्रफल (Sq.Ft) = Con Factor * Packing Unit
-    """
+def calculate_box_sqft(conv_factor, packing_unit):
+    # boxes ka kul kshetraphal (Sq.Ft) = Conv Factor * Packing Unit
     try:
-        cf = float(con_factor)
+        cf = float(conv_factor)
         pu = float(packing_unit)
         return cf * pu
     except Exception:
-        return 9.0
+        return 0.0
 
-def calculate_boxes(sqft, con_factor, packing_unit):
-    """
-    सटीक फॉर्मूला: आवश्यक बॉक्स = Sq.Ft / (Con Factor * Packing Unit)
-    """
+def calculate_boxes(sqft, conv_factor, packing_unit):
+    # aavshyak boxes = Sq.Ft / (Conv Factor * Packing Unit)
     try:
         sqft_val = float(sqft)
-        cf = float(con_factor)
-        pu = float(packing_unit)
-        
-        box_coverage = cf * pu
-        if box_coverage <= 0:
-            return 0
-            
-        # सीधे फॉर्मूले के अनुसार सीलिंग (Math.ceil) करके सही बॉक्स निकालना
-        return math.ceil(sqft_val / box_coverage)
+        box_sqft = calculate_box_sqft(conv_factor, packing_unit)
+        if box_sqft > 0:
+            # math.ceil se boxes hamesha agle poore number par round up honge
+            return math.ceil(sqft_val / box_sqft)
+        return 0
     except Exception:
         return 0
