0b729aa3fe48cc272ee206bdd1fbce1bc363c208
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-10 19:02:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -176,8 +176,7 @@ def delete_customer_db(cust_id):
     conn.close()
 
 # --- UNIVERSAL GOOGLE SHEET LOADER (EXACT HEADER MATCH) ---
-@st.cache_data(ttl=5)
-def get_master_df():
+@st.cache_data(ttl=3600)def get_master_df():
     try:
         raw_df = pd.read_csv(GOOGLE_SHEET_CSV_URL, header=None, dtype=str)
         h_idx = 0
