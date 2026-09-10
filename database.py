f0951a0cb3b459803727210c510b9b818b2b5b45
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-11 01:46:19 +0530

    Update Supabase URL and fix formatting issues

--- database.py
+++ database.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from supabase import create_client, Client
-SUPABASE_URL =  "https://gedzazirwxaxabnppchc"
+SUPABASE_URL = "https://gedzazriwvaxabnppchc.supabase.co"
 SUPABASE_KEY =  "sb_publishable_o18gTy66MVBCtq-DasQHAA_MlWvg_g"
 
 @st.cache_resource
