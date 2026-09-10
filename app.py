0e9c67b27db4dcac97282a4c0cdaea84f6ad96b7
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-11 00:18:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -149,7 +149,8 @@ if menu == "1. Customer Registration":
                             st.success("Customer deleted successfully!")
                             st.rerun()
                         else:
-                            st.error("Failed to delete customer.")elif menu == "2. Tile Selection & BOQ":
+                            st.error("Failed to delete customer.")
+                        elif menu == "2. Tile Selection & BOQ":
     st.title("Step 2: Area-wise Tile Selection")
     
     if not st.session_state['customer']:
