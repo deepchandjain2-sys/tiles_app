6b316a6265c00f371847715b60a33e7ba5c257d1
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-11 01:42:37 +0530

    Update Supabase URL format in database.py

--- database.py
+++ database.py
@@ -1,7 +1,6 @@
 import streamlit as st
 from supabase import create_client, Client
-
-SUPABASE_URL = "gedzazirwxaxabnppchc"
+SUPABASE_URL = "https://gedzazirwxaxabnppchc"
 SUPABASE_KEY = "sb_publishable_o18gTy66MVBCtq-DasQHAA_MlWvg_g"
 
 @st.cache_resource
