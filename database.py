fe5ba75cade4a650a0866b3b838627e7c613db58
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-12 00:34:50 +0530

    Refactor Supabase connection and customer functions

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ import streamlit as st
 from supabase import create_client, Client
 
 SUPABASE_URL = "https://gedzazirwxaxabnppchc.supabase.co"
-SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlZHphemlyd3hheGFibnBwY2hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEyMzg2Mzh9.EXAMPLE_KEY" # Yahan apni sirf wohi lamba wali key rakhein, piche ka text hata dein
+SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdlZHphemlyd3hheGFibnBwY2hjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg2ODAyOTgsImV4cCI6MjEwNDI1NjI5OH0.CSCbuwInWJtGpL7w_nMFU6ElGWnXxr67bKeMWuTpMMM"
 
 supabase: Client = None
 try:
