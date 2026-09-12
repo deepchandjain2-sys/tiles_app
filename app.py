7bdf06ffc6c40043023932346b894ff6ad9b1b5f
deepchandjain2-sys <deepchand.jain2@gmail.com>
2026-09-12 11:55:29 +0530

    Update Google Sheet URL and maintain functionality

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import math
 import os
 from database import get_all_customers, save_customer_to_db, delete_customer_from_db, get_all_admin_users
 
-GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vR4mWSP3s6r7UIwn-kcX8Ogev4yXWTMpMLvL87PGTR_UwxKjkcbU9NNxy__mbkyYplhDHxvsD2nKFvW/pub?gid=1816720040&single=true&output=csv"
+GOOGLE_SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRAMSp-l-7Ulm-KX80pqxVke8L87GTR_JckbGMwy-_WkYpTInHS02N4r-vV/pub?gid=0&single=true&output=csv"
 
 @st.cache_data(ttl=0)
 def load_catalog_from_google_sheet():
